5eadf56a067cfcd615fdedfcbf1495749d2a777d
임혜진 <hjlim7831@gmail.com>
2024-10-13 19:35:35 +0900

    refactor: 버튼 컴포넌트 변수명 변경, className props 추가

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,23 +6,25 @@ type Color = 'mint' | 'orange' | 'white';
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: Variant;
   color?: Color;
+  className?: string;
 };
 
 export const Button = ({
   color = 'orange',
   variant = 'contained',
+  className = '',
   children,
   disabled,
   ...props
 }: Props) => {
-  const baseClassName =
+  const backgroundClassName =
     variant === 'contained'
       ? `w-[311px] h-[41px] p-3 rounded-[6px] gap-1 flex items-center justify-center disabled:bg-grey-disabled ${colorMap[color].bg}`
       : '';
 
   const textClassName = `font-pretendard text-[14px] font-bold ${colorMap[color].text} leading-[16.71px] text-left`;
 
-  const finalClassName = `${baseClassName} ${textClassName}`;
+  const finalClassName = `${backgroundClassName} ${textClassName} ${className}`;
 
   return (
     <button className={finalClassName} {...props} disabled={disabled}>

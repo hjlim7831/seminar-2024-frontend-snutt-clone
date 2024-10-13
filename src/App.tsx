0ed2795489e5ef7e863407f8de0952632fe18ec9
임혜진 <hjlim7831@gmail.com>
2024-10-13 13:40:01 +0900

    feat: tailwindCSS 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './reset.css';
-
-import { useReducer } from 'react';
+import './index.css';
 
 export const App = () => {
-  const [count, increment] = useReducer((c: number) => c + 1, 0);
-
-  return <button onClick={increment}>{count}</button>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind with Vite!
+      </h1>
+    </div>
+  );
 };

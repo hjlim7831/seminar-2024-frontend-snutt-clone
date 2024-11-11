9825a509abe64e7c0ecc4ce02e1d33507acd204a
임혜진 <hjlim7831@gmail.com>
2024-11-11 22:26:57 +0900

    fix: 시간표 grid를 vh, vw 단위로 변경

--- src/views/AuthenticatedPage.tsx
+++ src/views/AuthenticatedPage.tsx
@@ -4,7 +4,7 @@ import { Navigation } from '../components/navigation';
 
 export const AuthenticatedPage = () => {
   return (
-    <div className="h-screen flex flex-col justify-center">
+    <div className="flex flex-col justify-center">
       <Outlet />
       <Navigation />
     </div>
